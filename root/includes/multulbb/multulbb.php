52417b9ae759fab093e280c768b0c7c988ebb332
Alt <unklefedor@gmail.com>
2011-03-12 00:35:46 +0300

    #

--- root/includes/multulbb/multulbb.php
+++ root/includes/multulbb/multulbb.php
@@ -2,9 +2,22 @@
 
 class MultulBB {
 
+        private function get_config() {
+    	  include ($phpbb_root_path . 'includes/multulbb/config');
+          
+          $config = array(
+              'multul_status'   =>  $config_multul_status,
+              'multul_id'       =>  $config_multul_id,
+              'multul_key'      =>  $config_multul_key,
+          );
+          return $config;
+        }
+
 	public static function multul_get_link($user_id, $username) {
 		global $auth, $user;
-		if ($user->data['is_registered'] && $user_id != $user->data['user_id']) {
+                $config = self::get_config();
+
+		if ($user->data['is_registered'] && $user_id != $user->data['user_id'] && $config['multul_status']) {
 			return '<a href="javascript:;" onclick="multul.im.openContact(' . $user_id . ',\'' . $username . '\')"><img src="http://multul.ru/media/images/messenger/chats.gif" alt="" /></a><br>';
 		} else {
 			return '';
@@ -13,19 +26,18 @@ class MultulBB {
 
 	public static function multul_init() {
 		global $user, $auth;
-
 		include($phpbb_root_path . 'includes/multulbb/lib/multul_lib.php');
-		include($phpbb_root_path . 'includes/multulbb/config');
+                $config = self::get_config();
 
 		$user_id		= $user->data['user_id'];
 		$user_name		= $user->data['username'];
-		$multul_id		= $config_multul_id;
-		$multul_key		= $config_multul_key;
+		$multul_id		= $config['multul_id'];
+		$multul_key		= $config['multul_key'];
 
-		if ($user->data['is_registered'] && $multul_id && $multul_key) {
+		if ($user->data['is_registered'] && $multul_id && $multul_key && $config['multul_status']) {
 			$config = array(
 				'app_id'		=> $multul_id,
-				'secret_key'	=> $multul_key,
+				'secret_key'            => $multul_key,
 				'uid'			=> $user_id,
 				'name'			=> $user_name,
 			);
